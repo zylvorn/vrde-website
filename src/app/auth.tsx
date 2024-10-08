726ec574ecbf4f669c706993e1945ed6f7ff741d
noobMaster <Randika.Alditia@xapiens.id>
2024-10-08 12:30:34 +0700

    fixing router

--- src/app/auth.tsx
+++ src/app/auth.tsx
@@ -1,10 +1,16 @@
 'use client'
-import React from 'react'
+import React, { useEffect } from 'react'
+import { usePathname, useRouter } from 'next/navigation'
 
 type TProps = {
   children?: React.ReactNode
 }
 const AuthLayout: React.FC<TProps> = ({ children }) => {
+  const router = useRouter()
+  const pathname = usePathname()
+  useEffect(() => {
+    if (pathname === '/') router.push('/home')
+  }, [router, pathname])
   return children
 }
 export default AuthLayout
